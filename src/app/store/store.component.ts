141aa0d3e075f5fa862fb1d59a27f3ab15845a93
martin4mari <martinmariaaa@gmail.com>
2022-03-29 20:35:06 -0300

    v1

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Product } from './product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'store',
   templateUrl: 'store.component.html',
@@ -10,7 +13,11 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) {}
+  constructor(
+    private repository: ProductRepository,
+    private cart: Cart,
+    private router: Router
+  ) {}
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -33,9 +40,16 @@ export class StoreComponent {
     this.productsPerPage = Number(newSize);
     this.changePage(1);
   }
+
   get pageCount(): number {
-    return Math.ceil(this.repository
-    .getProducts(this.selectedCategory).length / this.productsPerPage)
-    }
-    
+    return Math.ceil(
+      this.repository.getProducts(this.selectedCategory).length /
+        this.productsPerPage
+    );
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
+  }
 }
