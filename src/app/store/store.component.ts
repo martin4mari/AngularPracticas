7a0b884b613e8a1ddfe836139b212bc13c9eea06
martin4mari <martinmariaaa@gmail.com>
2022-03-29 18:38:45 -0300

    v1

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { Product } from './product.model';
 import { ProductRepository } from '../model/product.repository';
 @Component({
-selector: 'store',
-templateUrl: 'store.component.html',
-})
-export class StoreComponent {
-constructor(private repository: ProductRepository) {}
-get products(): Product[] {
-    return this.repository.getProducts();
-}
-get categories(): string[] {
-return this.repository.getCategories();
-}
+    selector: 'store',
+    templateUrl: 'store.component.html',
+    })
+    export class StoreComponent {
+    public selectedCategory = null;
+    constructor(private repository: ProductRepository) {}
+    get products(): Product[] {
+    return this.repository.getProducts(this.selectedCategory);
+    }
+    get categories(): string[] {
+    return this.repository.getCategories();
+    }
+    changeCategory(newCategory?: string ) {
+    this.selectedCategory=newCategory;
+    }
 }
